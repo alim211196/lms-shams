1e7dc47f3584d52cdaac7245b49e2bad73b833cd
Alim-Oxcytech <alim.mohd@oxcytech.com>
2025-02-14 12:19:05 +0530

    .

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export", // Helps fix static export issues
   reactStrictMode: true,
-  experimental: { appDir: true }, // Ensure App Router is enabled
+  experimental: { appDir: true }, // Ensure App Router works correctly
 };
 
 export default nextConfig;
